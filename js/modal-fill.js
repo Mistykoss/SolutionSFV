39fca103f7d9292db4b3dfe42fcd6ced514a7842
DESKTOP-6L08L2B\MystT <toimaxmermer12345@gmail.com>
2022-12-11 09:12:05 -0600

    updte

---
+++ js/modal-fill.js
@@ -0,0 +1,22 @@
+const servicesConteiner = document.getElementById("service-description");
+const itemExplore = document.getElementById("explore__items");
+console.log(itemExplore);
+//servicesConteiner.src = "/projects/main.html";
+
+itemExplore.addEventListener("click", function (e) {
+  console.log(e.target.dataset.project);
+  
+});
+console.log("se agredo");
+//se a some element here
+ function colorChange(color){
+  if (true){
+    
+  }
+
+
+
+
+
+
+ }
